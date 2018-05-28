0f60eda3c1093a460bdd4582120c2ab14225d68a
Michael <michaelalonsoo18@gmail.com>
2018-05-28 14:13:37 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -26,7 +26,7 @@ function onDeviceReady(){
 		'Altitude Accuracy: '   + position.coords.altitudeAccuracy +'\n'+
 		'Heading: '   + position.coords.heading +'\n'+
 		'Speed: '   + position.coords.speed +'\n'+
-		'Timestamp: '   + position.coords.timestamp +'\n');
+		'Timestamp: '   + position.timestamp +'\n');
 	 };
 	 
 	 function onError(error){
@@ -48,7 +48,7 @@ function onDeviceReady(){
 		 $('#aaccuracy').html(position.coords.altitudAccuracy);
 		 $('#headingg').html(position.coords.heading);
 		 $('#speed').html(position.coords.speed);
-		 $('#timestamp').html(position.coords.timestamp);
+		 $('#timestamp').html(position.timestamp);
 	 };
 	 
 	 function onError(error){
